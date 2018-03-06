749bc428fa39118bb3d126df3d6fffe0e9859f0e
Sami Koskivaara <Sami.Koskivaara@fi.fujitsu.com>
2018-03-06 12:54:36 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -10,8 +10,9 @@ def root():
 
     message = {
         "title": "This is a test flask application",
-        "version": "1.3",
+        "version": "1.4",
         "changes": [
+            "1.4: fix",
             "1.3: deploy",
             "1.2: cached builder for faster build time",
             "1.1: message changed",
