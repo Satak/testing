51494951cc41b9ab006b00de31413821f5063ad9
Sami Koskivaara <Sami.Koskivaara@fi.fujitsu.com>
2018-03-07 00:45:55 +0200

    html added

--- src/main.py
+++ src/main.py
@@ -1,36 +1,33 @@
 """Test application"""
 
-from os import environ
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
-TAG = environ.get('TAG')
-
-@app.route("/")
-def root():
-    """Main."""
-
-    message = {
+def message_ctrl():
+    return {
         "title": "This is a test flask application.",
         "version": "1.9",
-        "tag": TAG,
         "changes": [
-            "1.9: tag name added to cloudbuild.yaml",
-            "1.8: time changed",
-            "1.7: status added",
-            "1.6: revision id",
-            "1.5: after init deployment",
-            "1.4: fix",
-            "1.3: deploy",
-            "1.2: cached builder for faster build time",
-            "1.1: message changed",
-            "1.0: init commit"
+            {"1.9": "tag name added to cloudbuild.yaml"},
+            {"1.8": "time changed"},
+            {"1.0": "init commit"}
         ],
         "status": "production",
         "time": "06.03.2018 23:56"
     }
 
-    return jsonify(message)
+@app.route("/")
+def root():
+    """UI."""
+
+    return render_template('index.html', data=message_ctrl())
+
+@app.route("/api/version")
+def api_version():
+    """Api version."""
+
+    return jsonify(message_ctrl())
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
